ab8fbf511774ec0bfb9b64580d7ce694dc2f7a3d
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-02-23 21:58:39 -0300

    Se agregan las entidades con sus relaciones

---
+++ src/Blog.Api/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Api.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public ICollection<Post> Posts { get; set; } = [];
+    public  ICollection<Comment> Comments { get; set; } = [];
+}
---
+++ src/Blog.Api/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Api.Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsApproved { get; set; } = true; 
+
+
+    public int PostId { get; set; }
+    public Post Post { get; set; } = null!;
+
+    public string UserId { get; set; } = string.Empty;
+    public ApplicationUser User { get; set; } = null!;
+}
