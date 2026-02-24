132d0b11840f4e9d2debf538a7d15a2cf164b958
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-02-24 00:45:22 -0300

    D.I de Indentity core, configuraciones y seed de roles

---
+++ src/Blog.Api/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Api.Data;
+
+public class DbInitializer
+{
+    public static async Task SeedRoles(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        
+        var roleNames = Enum.GetNames<Roles>();
+
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+    }
+}
