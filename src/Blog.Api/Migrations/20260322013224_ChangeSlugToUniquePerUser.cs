f6e52138b1766e40d7d26f789449814e020cfc50
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-03-21 22:35:36 -0300

    migracion para que los slug de post sean unicos por usuarios

---
+++ src/Blog.Api/Migrations/20260322013224_ChangeSlugToUniquePerUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSlugToUniquePerUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_posts_author_id",
+                table: "posts");
+
+            migrationBuilder.DropIndex(
+                name: "ix_posts_slug",
+                table: "posts");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_author_id_slug",
+                table: "posts",
+                columns: new[] { "author_id", "slug" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_posts_author_id_slug",
+                table: "posts");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_author_id",
+                table: "posts",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_slug",
+                table: "posts",
+                column: "slug",
+                unique: true);
+        }
+    }
+}
